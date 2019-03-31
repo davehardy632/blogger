d360024ca1b533232314ad063831fab31c576471
David Hardy <davehardy632@gmail.com>
2019-03-31 10:46:38 -0600

    Created new test for show page to add a new comment to an article

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -19,4 +19,19 @@ describe "As a visitor" do
       expect(page).to have_content(comment_2.body)
     end
   end
+  describe "they fill in a comment form" do
+    it "displays the comment on the article show page" do
+      article = Article.create!(title: "New Title", body: "New Body")
+
+      visit article_path(article)
+
+      fill_in "comment[author_name]" with: "Me!"
+      fill_in "comment[body]" with: "So many thoughts on this article."
+      click_on "Submit"
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("Me!")
+      expect(page).to have_content("So many thoughts on this article.")
+  end
 end
