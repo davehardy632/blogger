348f3d6e8c11c8e6d8a95cc91c712c294b666b19
David Hardy <davehardy632@gmail.com>
2019-03-30 19:58:37 -0600

    Adds comments to article

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -4,13 +4,19 @@ describe "As a visitor" do
   describe "when I visit /authors" do
     it "I can click the title and see article show page" do
       article = Article.create!(title: "Article Title", body: "This is the body of the article where you can write all of the content")
-
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
       visit articles_path
 
       click_link article.title
 
+
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
