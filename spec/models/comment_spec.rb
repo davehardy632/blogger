8381647fb8758bc4344584d5e99e36aecf27bd13
David Hardy <davehardy632@gmail.com>
2019-03-30 18:49:11 -0600

    Set up relationship between articles and comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Comment, type: :model do
   describe "validations" do
+    it {should belong_to(:article)}
     it {should validate_presence_of(:author_name)}
     it {should validate_presence_of(:body)}
-    it {should belong_to(:article)}
   end
 end
