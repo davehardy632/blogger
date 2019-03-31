9b364bd411f4acba4a74784e986a411a4eaadd09
David Hardy <davehardy632@gmail.com>
2019-03-31 16:34:43 -0600

    Added links and show page, ability to delete tags.

--- spec/features/tags_index_spec.rb
+++ spec/features/tags_index_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-describe "As a visitor, when i visit /tags" do
-  describe 'I see all available tags for article subjects' do
-    it 'tag names are shown on the page' do
+RSpec.describe "As a visitor, when i visit /tags" do
+  describe "I see all available tags for article subjects" do
+    it "tag names are shown on the page" do
+
       article = Article.create!(title: "Very Interesting Article", body: "Even more interesting body of such article")
       tag = article.tags.create!(name: "Sports")
       tag_2 = article.tags.create!(name: "Health")
@@ -20,6 +21,19 @@ describe "As a visitor, when i visit /tags" do
       expect(page).to have_content(tag_4.name)
       expect(page).to have_content(tag_5.name)
       expect(page).to have_content(tag_6.name)
+    end
+    describe "I see all available tags" do
+      it "links tag names to show page" do
+        article = Article.create!(title: "Very Interesting Article", body: "Even more interesting body of such article")
+        tag = article.tags.create!(name: "Sports")
+
+        visit tags_path
+        click_on tag.name
+
+        expect(current_path).to eq(tag_path(tag))
+        expect(page).to have_content(tag.name)
+        expect(page).to have_content(article.title)
+      end
     end
   end
 end
