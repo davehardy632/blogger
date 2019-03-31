29608c2dffb44e99f9782afe947399cd5941baf5
David Hardy <davehardy632@gmail.com>
2019-03-31 12:56:49 -0600

    Added class method to article, and instance method to tag model, being able to display name of the tag when to_s is called on a tag object.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-
 describe Tag, type: :model do
   describe "relationships" do
     it {should have_many(:taggings)}
     it {should have_many(:articles).through(:taggings)}
+  end
+  describe "instance methods" do
+    it "should turn call name on tag objects" do
+      article = Article.create(title: "Tall Tables", body: "They are tough for the short legged")
+      tag_1 = article.tags.create(name: "furniture")
+      tag_2 = article.tags.create(name: "opinions")
+
+      expect(article.tag_list).to eq("furniture, opinions")
+      expect(tag_1.to_s).to eq(tag_1.name)
+      expect(tag_2.to_s).to eq(tag_2.name)
+    end
   end
 end
